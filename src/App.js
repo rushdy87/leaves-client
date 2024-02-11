1435740320609e3a45c8f10a8fe05036df88d585
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-02-11 11:11:00 +0300

    navigation to log in, when there is no user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useContext } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
 import Login from './pages/Login/Login';
-import { AuthContextProvider } from './context/auth-context';
+import { AuthContext } from './context/auth-context';
 
 function App() {
-  return (
-    <AuthContextProvider>
-      <Login />
-    </AuthContextProvider>
+  const { user } = useContext(AuthContext);
+
+  console.log(user);
+
+  const renderRoutes = user ? (
+    <Routes>
+      <Route path='/' element={<h1>Hi there</h1>} />
+      <Route path='*' element={<Navigate to='/' replace />} />
+    </Routes>
+  ) : (
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='*' element={<Navigate to='/login' replace />} />
+    </Routes>
   );
+
+  return <Router>{renderRoutes}</Router>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
+
+import { AuthContextProvider } from './context/auth-context';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
